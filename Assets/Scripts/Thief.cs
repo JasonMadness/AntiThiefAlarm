6c250d6870259c8cab50fabbdd6ca1d8de317e3d
JasonMadness <tobc_madness@mail.ru>
2025-06-02 16:46:41 +0300

    thief class refactoring

--- Assets/Scripts/Thief.cs
+++ Assets/Scripts/Thief.cs
@@ -3,33 +3,31 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class Thief : MonoBehaviour
 {
-    [SerializeField] private Chest _chest;
+    [SerializeField] private Transform _target;
     [SerializeField] private Transform _exit;
     [SerializeField] private float _speed;
-
-    private SpriteRenderer _spriteRenderer;
-    private Transform _target;
-
-    private void Awake()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+    [SerializeField] private float _flipRotationAngle = 180f;
+    
+    private Quaternion _flippedRotation;
 
     private void Start()
     {
-        _target = _chest.transform;
+        _flippedRotation = Quaternion.Euler(transform.rotation.x, _flipRotationAngle, transform.rotation.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.TryGetComponent(out Chest chest))
+        {
+            chest.Open();
+            _target = _exit;
+            transform.rotation = _flippedRotation;
+        }
     }
 
     private void Update()
     {
         float targetPositionX = Mathf.MoveTowards(transform.position.x, _target.position.x, _speed * Time.deltaTime);
         transform.position = new Vector2(targetPositionX, transform.position.y);
-
-        if (transform.position.x == _chest.transform.position.x)
-        {
-            _target = _exit;
-            _spriteRenderer.flipX = false;
-            _chest.Open();
-        }
     }
 }
